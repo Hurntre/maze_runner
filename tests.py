9f413d860006ceeabc37db0a0d2752784dbd17af
Adefolaju <adefolajuariyo@gmail.com>
2024-07-05 09:38:07 +0100

    break down walls in the maze

--- tests.py
+++ tests.py
@@ -16,17 +16,17 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
-    def test_maze_create_cells_2(self):
-        num_cols = 20
-        num_rows = 30
-        maze_one = Maze(0,0, num_rows, num_cols, 10, 10)
+    def test_maze_break_entrance_and_exit(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
-            len(maze_one._cells),
-            num_cols,
+            m1._cells[0][0].has_top_wall,
+            False,
         )
         self.assertEqual(
-            len(maze_one._cells[0]),
-            num_rows,
+            m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
+            False,
         )
 
 if __name__ == "__main__":
