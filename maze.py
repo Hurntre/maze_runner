cc0dcb474761df9c56cd2f56204e819a5db25b74
Adefolaju <adefolajuariyo@gmail.com>
2024-07-05 09:35:20 +0100

    write tests for maze class

--- maze.py
+++ maze.py
@@ -11,7 +11,7 @@ class Maze:
       num_cols,
       cell_size_x,
       cell_size_y,
-      win
+      window=None
     ):
     self._x1 = x1
     self._y1 = y1
@@ -19,7 +19,7 @@ class Maze:
     self._num_cols = num_cols
     self._cell_size_x = cell_size_x
     self._cell_size_y = cell_size_y
-    self._win = win
+    self._window = window
     self._cells = []
 
     self._create_cells()
@@ -29,14 +29,14 @@ class Maze:
     for i in range(self._num_cols):
         col_cells = []
         for j in range(self._num_rows):
-            col_cells.append(Cell(self._win))
+            col_cells.append(Cell(self._window))
         self._cells.append(col_cells)
     for i in range(self._num_cols):
         for j in range(self._num_rows):
             self._draw_cell(i, j)
 
   def _draw_cell(self, i, j):
-      if self._win is None:
+      if self._window is None:
           return
       x1 = self._x1 + i * self._cell_size_x
       y1 = self._y1 + j * self._cell_size_y
@@ -46,7 +46,7 @@ class Maze:
       self._animate()
 
   def _animate(self):
-      if self._win is None:
+      if self._window is None:
           return
-      self._win.redraw()
+      self._window.redraw()
       time.sleep(0.05)
